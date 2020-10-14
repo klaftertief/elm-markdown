2e9758709601669d06e205edfdebb2926196479a
klaftertief <jonas.coch@itravel.de>
2020-10-14 22:38:00 +0200

    Update htmlRenderer for spec tests

--- spec-tests/OutputMarkdownHtml.elm
+++ spec-tests/OutputMarkdownHtml.elm
@@ -116,7 +116,7 @@ renderMarkdown markdown =
             , text =
                 Html.text
             , unorderedList =
-                \isLoose items ->
+                \items ->
                     Html.ul []
                         (items
                             |> List.map
@@ -144,20 +144,12 @@ renderMarkdown markdown =
                                                                 , Attr.type_ "checkbox"
                                                                 ]
                                                                 []
-
-                                                wrappedChildren =
-                                                    case isLoose of
-                                                        Block.IsLoose ->
-                                                            [ Html.p [] children ]
-
-                                                        Block.IsTight ->
-                                                            children
                                             in
-                                            Html.li [] (checkbox :: wrappedChildren)
+                                            Html.li [] (checkbox :: children)
                                 )
                         )
             , orderedList =
-                \startingIndex isLoose items ->
+                \startingIndex items ->
                     Html.ol
                         (if startingIndex /= 1 then
                             [ Attr.start startingIndex ]
@@ -169,13 +161,7 @@ renderMarkdown markdown =
                             |> List.map
                                 (\itemBlocks ->
                                     Html.li []
-                                        (case isLoose of
-                                            Block.IsLoose ->
-                                                [ Html.p [] itemBlocks ]
-
-                                            Block.IsTight ->
-                                                itemBlocks
-                                        )
+                                        itemBlocks
                                 )
                         )
             , html =
